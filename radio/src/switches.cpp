2625528daf08174f26a69992bb1a41c92c16cf21
Wastl Kraus <derdoktor667@gmail.com>
2019-06-19 21:47:40 +0200

    Update switches.cpp

--- radio/src/switches.cpp
+++ radio/src/switches.cpp
@@ -725,21 +725,14 @@ void checkSwitches() {
     LED_ERROR_BEGIN();
 
     // first - display warning
-#if defined(PCBTARANIS) || defined(PCBHORUS) || defined(PCBFLYSKY) || \
-    defined(PCBNV14)
-    int x;
-    int y;
+#if defined(PCBTARANIS) || defined(PCBHORUS) || defined(PCBFLYSKY)
     if ((last_bad_switches != switches_states) || (last_bad_pots != bad_pots)) {
       drawAlertBox(STR_SWITCHWARN, NULL, STR_PRESSANYKEYTOSKIP);
       if (last_bad_switches == 0xff || last_bad_pots == 0xff) {
         AUDIO_ERROR_MESSAGE(AU_SWITCH_ALERT);
       }
-
-      x = SWITCH_WARNING_LIST_X - SWITCH_WARNING_LIST_X_OFFSET;
-      y = SWITCH_WARNING_LIST_Y;
+      int x = SWITCH_WARNING_LIST_X, y = SWITCH_WARNING_LIST_Y;
       int numWarnings = 0;
-
-      LCD_SWITCH_WARN_BACKGROUND(1, y);
       for (int i = 0; i < NUM_SWITCHES; ++i) {
 #if defined(COLORLCD)
         if (SWITCH_WARNING_ALLOWED(i)) {
